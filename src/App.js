464a714c650c2aa2150a5c54de69a1e9483ead53
chandzul <dchandzul@gmail.com>
2021-01-26 23:09:51 -0600

    concepto basico de crear componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+// import { Component } from 'react'
 import './App.css';
 
-function App() {
+function Helloworld({mytext, subtitle}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="hello">
+      <h3>{subtitle}</h3>
+      {mytext}
     </div>
   );
 }
+
+function App() {
+  return (
+    <div>
+      this is my component:
+      <Helloworld mytext="Hello David" subtitle="Subititulo"/>
+      <Helloworld mytext="hola mundo" subtitle="Componente 2"/>
+      <Helloworld mytext="hello!!!"/>
+    </div>
+  );
+}
+
+// class App extends Component{
+//   render() {
+//     return <div> This is my component <Helloworld/> </div>
+//   }
+// }
+
+// const App = () => <div>This is my componet: <Helloworld/> </div>;
 
 export default App;

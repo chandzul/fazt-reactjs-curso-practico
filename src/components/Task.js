0e82729636d891345812d26b9882f545ff238c25
chandzul <dchandzul@gmail.com>
2021-01-28 00:54:27 -0600

    agregando estilos en lines por objetos

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,40 @@
 import React, { Component } from 'react';
+import './Task.css';
 
 class Task extends Component {
-  render() {
 
+  StyleCompleted() {
+    return {
+      fontSize: '20px',
+      color: this.props.task.done ? 'gray' : 'black',
+      textDecoration: this.props.task.done ? 'line-through' : 'none'
+    }
+  }
+
+  render() {
     const { task } = this.props;
 
     return(
-      <div className="task">
+      <div className="task" style={this.StyleCompleted()}>
           { task.title } - { task.description } - { task.done } - { task.id }
           <input type="checkbox"/>
-          <button>
+          <button style={btnDelete}>
             x
           </button>
       </div>
     );
   }
 }
+
+const btnDelete = {
+  fontSize: '18px',
+  background: '#ea2027',
+  color: '#ffffff',
+  border: 'none',
+  padding: '10px 15px',
+  borderRadius: '50%',
+  cursor: 'pointer'
+};
 
 export {
   Task
